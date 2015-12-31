e3224fe15158f23a5a226275c97e7ca0bb370279
BarryChew <businessatbarry@gmail.com>
2015-12-31 11:37:09 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,5 +9,6 @@
   
   <h2> Starting at only $5 a month </h2>
   <p class="price-starting-desc">Receive a free domain name, unlimited bandwidth and 100GB of storage!</p>
+  <p class="more-desc">Allowed up to 5 MySQL Accounts.</p>
 </body>
 </html>
