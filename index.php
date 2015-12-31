dcaac035246c829ddfd209b45a21027208c990f7
BarryChew <businessatbarry@gmail.com>
2015-12-31 11:42:44 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   <p class="h1-desc">Start building your website today!</p>
   <br>
   
-  <h2> Starting at only $5 a month </h2>
+  <h2> Starting at only <b class="starting-price">$5</b> a month </h2>
   <p class="price-starting-desc">Receive a free domain name, unlimited bandwidth and 100GB of storage!</p>
   <p class="more-desc">Allowed up to 5 MySQL Accounts.</p>
 </body>
